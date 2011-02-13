e4e1701b099a7b324b65b432c727cec3fc3277b5
Safturento <Safturento@gmail.com>
2011-02-13 10:13:11 -0600

    Add border coloring to dispellable buffs on enemy targets

--- Tukui/core/functions.lua
+++ Tukui/core/functions.lua
@@ -636,6 +636,12 @@ T.PostUpdateAura = function(icons, unit, icon, index, offset, filter, isDebuff, 
 			icon:SetBackdropBorderColor(color.r * 0.6, color.g * 0.6, color.b * 0.6)
 			icon.icon:SetDesaturated(false)
 		end
+	else
+		if (isStealable or ((TukuiDB.myclass == "MAGE" or TukuiDB.myclass == "PRIEST" or TukuiDB.myclass == "SHAMAN") and dtype == "Magic")) and not UnitIsFriend("player", unit) then
+			icon:SetBackdropBorderColor(1, 0.85, 0)
+		else
+			icon:SetBackdropBorderColor(unpack(TukuiCF.media.bordercolor))
+		end
 	end
 	
 	if duration and duration > 0 then
